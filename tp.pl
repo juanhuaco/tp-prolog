294cde9411e541c5b8fca86a6b43cce01f28afbb
juanhuaco <juanpablohuacodelcarpio@gmail.com>
2021-11-03 14:04:01 -0300

    ya funca el 2

--- tp.pl
+++ tp.pl
@@ -1,46 +1,33 @@
 :-use_rendering(table).
 
-/*EJERCICIO 1*/
-
+/*============EJERCICIO 1===============*/
 
 tableroInicial(T):-
     T = [[-,-,-,-,-,-,-],[-,-,-,-,-,-,-],[-,-,-,-,-,-,-],[-,-,-,-,-,-,-],[-,-,-,-,-,-,-],[-,-,-,-,-,-,-]].
 
-/*EJERCICIO 2*/
-/**/
-ingresarFicha([F1, F2|Fs], Columna, Ficha, [T1, T2|Ts]):- Fs\=[],
+/*===========EJERCICIO 2================*/
+
+/*haya una ficha abajo - remplazamos*/
+ingresarFicha([F1, F2|Fs], Columna, Ficha, [T1, T2|Ts]):-
     posicion(F1, Columna, Ret1), Ret1='-',
-    posicion(F2, Columna, Ret2), Ret2\='-',
+    posicion(F2, Columna, Ret2), Ret2\='-',!,
     T2 = F2, Ts = Fs,
     remplazar(F1, Columna, Ficha, T1).
-
+/*no haya ficha - bajamos - evaluamos again*/
 ingresarFicha([F1, F2|[Fs1 | Fss]], Columna, Ficha, [T1, T2|[Ts1|Tss]]):-
     posicion(F1, Columna, Ret1), Ret1='-',
     posicion(F2, Columna, Ret2), Ret2='-',
     T1 = F1,
     ingresarFicha([F2, Fs1|Fss], Columna, Ficha, [T2, Ts1|Tss]).
-
+/*estamos es la ultima fila - Remplazamos*/
 ingresarFicha([F1, F2|[]], Columna, Ficha, [T1, T2|[]]):-
     posicion(F1, Columna, Ret1), Ret1='-',
     posicion(F2, Columna, Ret2), Ret2='-',
     T1 = F1,
     remplazar(F2, Columna, Ficha, T2).
 
-/*
-ingresarFicha(T, C, F, T2).
 
-ingresarFicha([T|Ts], C, F, T2) :- Ts=[], posicion(T, C, Pos), Pos = '-', T2
-
-1-estamos es la ultima fila - Remplazamos
-2-haya una ficha abajo - remplazamos
-3-no haya ficha - bajamos - evaluamos again
-4-este lleno en la fila 1 no hay espacio
-*/
-/** <examples> Your example queries go here, e.g.
-?- member(X, [cat, mouse]).
-*/
-
-/*EJERCICIO 4*/
+/*==================EJERCICIO 4======================*/
 
 contenido([T|Ts],[Y, X],Ficha):-
     longitud(Ts, L), 6 is L+Y,!, posicion(T, X, Ficha).
@@ -64,7 +51,7 @@ remplazar([Elemento|Resto], Posicion, Ficha, [R|Rs]):-
     R=Elemento,
     remplazar(Resto, Posicion, Ficha, Rs).
     
-/*Valores estaticos*/
+/*====================Valores estaticos=======================*/
 fila(1).
 fila(2).
 fila(3).
